9250807e2d13673b02e62667111e720e7a8211e4
carmenne <carmen.pomana04@gmail.com>
2024-12-11 18:30:55 +0100

    Trying to become a Pythonista

--- advent4_1.py
+++ advent4_1.py
@@ -1,54 +1,23 @@
-
 def dfs(chars, m, n, i, j, word, i_prev, j_prev, direction):
-
     if i >= m or j >= n or i < 0 or j < 0:
         return 0
 
-    if direction == "UP_LEFT" and not (i == i_prev - 1 and j  == j_prev - 1):
+    for dx, dy, dirct in dist:
+        if direction == dirct and not (i_prev + dx == i and j_prev + dy == j):
             return 0
-    elif direction == "UP" and not (i == i_prev - 1 and j == j_prev):
-        return 0
-    elif direction == "UP_RIGHT" and not (i == i_prev - 1 and j == j_prev + 1):
-        return 0
-    elif direction == "LEFT" and not (i == i_prev and j == j_prev - 1):
-        return 0
-    elif direction == "RIGHT" and not(i == i_prev and j == j_prev + 1):
-        return 0
-    elif direction == "BOTTOM_LEFT" and not(i == i_prev + 1 and j == j_prev - 1):
-        return 0
-    elif direction == "BOTTOM" and not (i == i_prev + 1 and j == j_prev):
-        return 0
-    elif direction == "BOTTOM_RIGHT" and not (i == i_prev + 1 and j == j_prev + 1):
-        return 0
 
     letter = chars[i][j]
     word += letter
 
-    if letter not in "XMAS":
+    if letter not in "XMAS" or len(word) > 4:
         return 0
-
-    if letter == "M" and word != "XM":
-        return 0
-
-    elif letter == "A" and word != "XMA":
-        return 0
-    elif letter == "S" and word != "XMAS":
-        return 0
-
-    if len(word) > 4:
+    expected_words = {"M":"XM", "A":"XMA", "S":"XMAS"}
+    if letter in expected_words and word != expected_words[letter]:
         return 0
 
     xmas_count = 1 if word == "XMAS" else 0
-
-    return xmas_count + \
-            + dfs(chars, m, n, i - 1, j - 1, word, i, j, "UP_LEFT" if direction == "" else direction) \
-            + dfs(chars, m, n, i - 1, j, word, i, j, "UP" if direction == "" else direction) \
-            + dfs(chars, m, n, i - 1, j + 1, word, i, j, "UP_RIGHT" if direction == "" else direction) \
-            + dfs(chars, m, n, i, j - 1, word, i, j, "LEFT" if direction == "" else direction) \
-            + dfs(chars, m, n, i, j + 1, word, i, j, "RIGHT" if direction == "" else direction) \
-            + dfs(chars, m, n, i + 1, j - 1, word, i, j, "BOTTOM_LEFT" if direction == "" else direction) \
-            + dfs(chars, m, n, i + 1, j, word, i, j, "BOTTOM" if direction == "" else direction) \
-            + dfs(chars, m, n, i + 1, j + 1, word, i, j, "BOTTOM_RIGHT" if direction == "" else direction)
+    return xmas_count + sum(dfs(chars, m, n, i + dx, j + dy, word, i, j, dirct if direction == "" else direction)
+                            for dx, dy, dirct in dist)
 
 
 letters = []
@@ -56,12 +25,15 @@ with open("input.txt", "r") as file:
     for line in file:
         letters.append(list(line.replace("\n", "")))
 
-    xmas = 0
     row = len(letters)
-    column = len(letters[0])
-    for x in range(row):
-        for y in range(column):
-            if letters[x][y] == "X": # top-left of the form
-                xmas += dfs(letters, row, column, x, y, "",  x, y, "")
+    col = len(letters[0])
+    dist = {(-1, -1, "UP_LEFT"), (-1, 0, "UP"), (-1, 1, "UP_RIGHT"), (0, -1, "LEFT"), (0, 1, "RIGHT"),
+            (1, -1, "BOTTOM_LEFT"), (1, 0, "BOTTOM"), (1, 1, "BOTTOM_RIGHT")}
+
+    xmas = sum(
+        dfs(letters, row, col, x + dx, y + dy, "X", x, y, direction)
+        for x in range(row)
+        for y in range(col) if letters[x][y] == "X"
+        for dx, dy, direction in dist)
 
     print(xmas)
