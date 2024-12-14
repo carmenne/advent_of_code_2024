bbede7e03be6bead80d15edc8463f101a3bb76b9
carmenne <carmen.pomana04@gmail.com>
2024-12-14 14:22:39 +0100

    Update advent12_2.py

--- advent12_2.py
+++ advent12_2.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 plants = []
 with open("input.txt", "r") as file:
     plants += [list(line.strip()) for line in file]
@@ -6,53 +8,31 @@ m = len(plants)
 n = len(plants[0])
 
 dist = {(-1, 0, "D"), (0, -1, "L"), (0, 1, "R"), (1, 0, "U")}
-reverse = {(-1, 0): "U", (0, -1): "L", (0, 1): "R", (1, 0): "D"}
 new_dist = {(0, -1, "L"), (1, 0, "U")}
+
 
 def in_bounds(i, j):
     return 0 <= i < m and 0 <= j < n
 
 
-def area(i, j, visited, plant, directions):
+def area(i, j, plant):
     if i >= m or j >= n or i < 0 or j < 0:
-        return 0, 0
+        return 0
 
     if visited[i][j]:
-        return 0, 0
-
-    for dx, dy, d in dist:
-        if not in_bounds(i + dx, j + dy):
-            if (i, j) in directions:
-                directions[i, j].add(reverse[(dx, dy)])
-            else:
-                directions[i, j] = {reverse[(dx, dy)]}
-
-        elif plants[i][j] != plants[i + dx][j + dy]:
-            if (i, j) in directions:
-                directions[i, j].add(reverse[(dx, dy)])
-            else:
-                directions[i, j] = {reverse[(dx, dy)]}
+        return 0
 
     visited[i][j] = True
     count = 1
-    fences = 0
 
     for dx, dy, d in dist:
-        x = i + dx
-        y = j + dy
-        if not in_bounds(x, y) or plants[x][y] != plant:
-            fences += 1
+        nx, ny = i + dx, j + dy
+        if not in_bounds(nx, ny) or plants[i][j] != plants[nx][ny]:
+            directions[(i, j)].add(d)
+        if in_bounds(nx, ny) and plants[nx][ny] == plant:
+            count += area(nx, ny, plant)
 
-        else:
-            sub_count, sub_fence = area(x, y, visited, plant, directions)
-            count += sub_count
-            fences += sub_fence
-
-    return count, fences
-
-
-visited = [[False for _ in range(m)] for _ in range(n)]
-total = 0
+    return count
 
 
 def none(pot, neighbours):
@@ -64,30 +44,27 @@ def none(pot, neighbours):
 
 
 def count_distinct():
-
-    new_set = set()
-
+    count = 0
     for k in range(0, m):
         for l in range(0, n):
             if (k, l) in directions:
+                neighbours = {
+                    (k + dx, l + dy, nd)
+                    for dx, dy, _ in new_dist
+                    if (k + dx, l + dy) in directions
+                    for nd in directions[k + dx, l + dy]
+                }
 
-                neighbours = set()
-                for dx, dy, _ in new_dist:
-                    if (k + dx, l + dy) in directions:
-                        for nd in directions[k + dx, l + dy]:
-                            neighbours.add((k + dx, l + dy, nd))
-                            # check
-                for d in directions[(k, l)]:
-                    if none((k, l, d), neighbours):
-                        new_set.add((k, l, d))
-
-    return len(new_set)
+                count += sum(1 for d in directions[(k, l)] if none((k, l, d), neighbours))
+    return count
 
 
+visited = [[False for _ in range(m)] for _ in range(n)]
+total = 0
 for i in range(m):
     for j in range(n):
-        directions = {}
-        region_count, region_fence = area(i, j, visited, plants[i][j], directions)
+        directions = defaultdict(set)
+        region_count = area(i, j, plants[i][j])
         if region_count > 0:
             distinct = count_distinct()
             total += region_count * distinct
