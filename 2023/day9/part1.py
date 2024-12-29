285dc8f80a5d96462ada47a76104157f26a6958e
carmenne <carmen.pomana04@gmail.com>
2024-12-29 17:20:09 +0100

    Update part1.py

--- 2023/day9/part1.py
+++ 2023/day9/part1.py
@@ -1,31 +1,14 @@
-histories = []
-with open("input_long.txt", "r") as file:
-    for line in file:
-        histories.append([int(number) for number in line.strip().split(" ")])
-
-def not_zero(difference):
-    return difference and difference != {0}
-
-next_elements = []
-for history in histories:
-
-    k = 1
-    not_all_zeroes = True
+def forecast(history):
+    k, last = 1, 0
     history_it = history
-    last = []
-    length = len(history)
-    while not_all_zeroes:
-
-        difference = []
-        for i in range(1, length - k + 1):
-            dif = history_it[i] - history_it[i-1]
-            difference.append(dif)
-        history_it = difference
-        last.append(difference[-1])
-
+    while set(history_it) != {0}:
+        history_it = [history_it[i] - history_it[i - 1] for i in range(1, len(history) - k + 1)]
+        last += history_it[-1]
         k += 1
-        not_all_zeroes = not_zero(set(difference))
 
-    next_elements.append(history[-1] + sum(last))
+    return history[-1] + last
 
-print(sum(next_elements)) # 1993300041
+
+with open("input_long.txt", "r") as file:
+    print(sum(forecast([int(number) for number in line.strip().split(" ")]) for line in file))
+    # 1993300041
