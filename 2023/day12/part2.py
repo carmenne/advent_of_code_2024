68a5d3e7100fe448f14c3981af2a45e291f81f23
carmenne <carmen.pomana04@gmail.com>
2025-01-02 07:51:15 +0100

    Update part2.py

--- 2023/day12/part2.py
+++ 2023/day12/part2.py
@@ -7,34 +7,43 @@ all_counts = []
 with open("input_long.txt", "r") as file:
     for line in file:
         token, cts = line.strip().split(" ")
-        tokens.append(token.strip("."))
-        all_counts.append([int(c) for c in cts.split(",")])
+        strip = token.strip("")
+
+        tokens.append("?".join([strip]*5))
+        all_counts.append([int(c) for c in cts.split(",")]*5)
 
 total = 0
 
-seen = set()
-
-def get_len(counts):
-    return sum(counts) + len(counts) - 1
-
-def valid(token, counts):
-    sub_tokens = [s_t for s_t in token.split(".") if s_t != ""]
-    if len(sub_tokens) != len(counts):
-        return False
-
-    sub_tokens_len = [len(t) for t in sub_tokens]
-    return sub_tokens_len == counts
-
 memo={}
 def solve(token, counts):
-    if "?" not in token:
-        return 1 if valid(token, counts) else 0
+    if not counts:
+        return 1 if "#" not in token else 0
+    if not token:
+        return not counts
 
-    result = 0
-    i = token.find("?")
-    solve1 = solve(token[:i] + "." + token[i + 1:], counts)
-    solve2 = solve(token[:i] + "#" + token[i + 1:], counts)
-    result += solve1 + solve2
+    key = token, ",".join([str(c) for c in counts])
+    if key in memo:
+        return memo[key]
+
+    i = token[0]
+    if i == ".":
+        result = solve(token[1:], counts)
+    elif i == "#":
+        new_counts = counts.copy()
+        to_remove = counts[0]
+        if (len(token) >= to_remove
+                and "." not in token[0:to_remove]
+                and (len(token) == to_remove or token[to_remove] in ".?")):
+            new_counts.remove(to_remove)
+            result = solve(token[to_remove+1:], new_counts.copy())
+        else:
+            result = 0
+    else:
+        solve1 = solve("." + token[1:], counts)
+        solve2 = solve("#" + token[1:], counts)
+        result = solve1 + solve2
+
+    memo[token, ",".join([str(c) for c in counts])] = result
 
     return result
 
@@ -43,4 +52,4 @@ for token, counts in zip(tokens, all_counts):
     res = solve(token, counts)
     total += res
 
-print(total) # 7204
+print(total)  # 1672318386674
